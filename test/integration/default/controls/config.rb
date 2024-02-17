e9257745dd5c84f6ed79ab098de031b061d88a6a
jeanluc <lkubb@protonmail.com>
2024-02-17 19:10:57 +0100

    fix: Update template

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'electrs.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"electrs-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"electrs--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"electrs-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"electrs--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
